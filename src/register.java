102e043719a817cf6dab92b0e9d3c95d4e4991f1
Tuscano <rtuscano@MPKL138a3ec3a.local>
2016-12-24 20:21:15 -0800

    first commit

---
+++ src/register.java
@@ -0,0 +1,79 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class register
+ */
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public register() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+	
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String pass = request.getParameter("pass");
+        try{
+        
+        //loading drivers for mysql
+        Class.forName("com.mysql.jdbc.Driver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:mysql://myapp-registration.csp4seb6lgkn.us-west-2.rds.amazonaws.com:3306/registration","sa","intuit01");
+
+        PreparedStatement ps=con.prepareStatement
+                  ("insert into Student values(?,?,?)");
+
+        ps.setString(1, name);
+        ps.setString(2, email);
+        ps.setString(3, pass);
+        int i=ps.executeUpdate();
+        
+          if(i>0)
+          {
+            out.println("You are sucessfully registered");
+          }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+	
+      }
+
+}
